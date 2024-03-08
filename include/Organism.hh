5790b7301eec202d1af4338f1c0b0091af01cd85
SandersJack <sandersjack141@gmail.com>
2024-03-08 14:10:12 +0000

    Added organism

---
+++ include/Organism.hh
@@ -0,0 +1,19 @@
+#ifndef Organism_H
+#define Organism_H
+
+class Orgainism {
+
+    public:
+        Orgainism();
+        ~Orgainism();
+
+    private:
+
+        int fAge;
+        double fHunger;
+        double fHealth;
+
+};
+
+
+#endif
---
+++ src/Organism.cc
@@ -0,0 +1,8 @@
+#include "Organism.hh"
+
+Orgainism::Orgainism(): fAge(0), fHunger(100), fHealth(100) {
+
+}
+
+Orgainism::~Orgainism(){
+}

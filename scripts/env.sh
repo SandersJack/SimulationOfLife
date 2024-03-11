7c04963cba617dab10b986efe5a882516d113def
SandersJack <sandersjack141@gmail.com>
2024-03-11 11:31:09 +0000

    update env

--- scripts/env.sh
+++ scripts/env.sh
@@ -2,4 +2,4 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 DIR=$(dirname "$(dirname "${DIR}")")
 
 export SOLSOURCE="${DIR}/SimulationOfLife"
-alias SOL="${SOLSOURCE}/build/SimulationOfLife"
+alias SOL="${SOLSOURCE}/build/SoL"

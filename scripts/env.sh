efb61b2bb7723b4695d2e42e91b5abcf5fbc00e9
SandersJack <sandersjack141@gmail.com>
2024-03-12 15:51:49 +0000

    Updated doxy file to use dynamic paths

--- scripts/env.sh
+++ scripts/env.sh
@@ -2,4 +2,6 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 DIR=$(dirname "$(dirname "${DIR}")")
 
 export SOLSOURCE="${DIR}/SimulationOfLife"
+export DOC_OUTPUT_DIR="${SOLSOURCE}/doc"
+
 alias SOL="${SOLSOURCE}/build/SoL"

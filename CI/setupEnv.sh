03866edd1d9d10e3dfd7a23cfc1d2d845397a8eb
SandersJack <sandersjack141@gmail.com>
2024-03-14 13:36:56 +0000

    update to actions

--- CI/setupEnv.sh
+++ CI/setupEnv.sh
@@ -5,3 +5,5 @@ echo "Installing Packages"
 sudo apt-get install libgl1-mesa-dev
 sudo apt-get install libglew-dev
 sudo apt-get install libglfw3 libglfw3-dev
+
+git submodule update --init --recursive

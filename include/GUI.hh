ad278740a1ea0777aa5637e76c5c75e0feb54a56
SandersJack <sandersjack141@gmail.com>
2024-03-14 13:25:27 +0000

    Updates that allow compile and also the creation of a window. Probs will not work on MacOS

--- include/GUI.hh
+++ include/GUI.hh
@@ -1,10 +1,7 @@
 #ifndef GUI_H
 #define GUI_H
 
-#include <imgui.h>
-#include <imgui_impl_glfw.h>
-#include <imgui_impl_opengl3.h>
-#include <GLFW/glfw3.h>
+#include "GLTools.hh"
 
 class GUI {
 
--- src/GUI.cc
+++ src/GUI.cc
@@ -17,52 +17,16 @@ GUI *GUI::GetInstance(){
 }
 
 void GUI::Init(){
-    glfwInit();
-    fWindow = glfwCreateWindow(800, 600, "World Visualization", nullptr, nullptr);
-    glfwMakeContextCurrent(fWindow);
+    int SCR_WIDTH = 1024;
+    int SCR_HEIGHT = 768;
 
-    IMGUI_CHECKVERSION();
-    ImGui::CreateContext();
-    ImGui_ImplGlfw_InitForOpenGL(fWindow, true);
-    ImGui_ImplOpenGL3_Init("#version 330");
+	OGLManager *oglM = OGLManager::GetInstance();
+	oglM->Init(SCR_WIDTH, SCR_HEIGHT);
+
+    fWindow = oglM->GetWindow();
 }
 
 void GUI::Draw(){
     glfwPollEvents();
-
-    // Start ImGui frame
-    ImGui_ImplOpenGL3_NewFrame();
-    ImGui_ImplGlfw_NewFrame();
-    ImGui::NewFrame();
-
-    // Create ImGui window
-    ImGui::Begin("Grid Visualization");
-
-    // Draw grid
-    const int gridSize = SimManager::GetInstance()->GetGridSize();
-    const float cellSize = 30.0f;
-    for (int i = 0; i < gridSize; ++i) {
-        for (int j = 0; j < gridSize; ++j) {
-            ImGui::GetWindowDrawList()->AddRect(ImGui::GetCursorScreenPos(),
-                                                    ImVec2(ImGui::GetCursorScreenPos().x + cellSize,
-                                                        ImGui::GetCursorScreenPos().y + cellSize),
-                                                    IM_COL32(255, 255, 255, 255));
-            ImGui::Dummy(ImVec2(cellSize, cellSize));
-            ImGui::SameLine();
-        }
-        ImGui::NewLine();
-    }
-
-    // End ImGui window
-    ImGui::End();
-
-    // Rendering
-    int display_w, display_h;
-    glfwGetFramebufferSize(fWindow, &display_w, &display_h);
-    glViewport(0, 0, display_w, display_h);
-    glClearColor(0.45f, 0.55f, 0.60f, 1.00f);
-    glClear(GL_COLOR_BUFFER_BIT);
-    ImGui::Render();
-    ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
-    glfwSwapBuffers(fWindow);
+    
 }
